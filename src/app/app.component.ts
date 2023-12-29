e50223ea0d8400869e522a052f34bd2b9213b6dc
Naoures <nawreswelhazi@hotmail.com>
2023-12-29 11:12:13 +0100

    salla7t scroll w dhahert eli meyodhrouch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,17 +13,8 @@ export class AppComponent implements OnInit {
 
   
   ngOnInit() {
-    
-    this.setupEventListeners();
-    this.zone.runOutsideAngular(() => {
-      window.addEventListener("scroll", () => {
-        this.handleScroll();
-        this.cdr.detectChanges();
-      });
-    });
     window.addEventListener("load", () => {
       this.handlePreloader();
-      this.revealElementOnScroll();
     });
 }
   
@@ -38,60 +29,6 @@ export class AppComponent implements OnInit {
           console.log('loadedddddddd');
         }
       }
-  // toggleNav() {
-  //   const navbar = this.renderer.selectRootElement("[data-navbar]");
-  //   const overlay = this.renderer.selectRootElement("[data-overlay]");
-
-  //   if (navbar && overlay) {
-  //     this.renderer.removeClass(navbar, 'active');
-  //     this.renderer.removeClass(overlay, 'active');
-  //     this.renderer.removeClass(document.body, 'nav-active');
-  //   }
-  // }
-  // activeElementOnScroll() {
-  //   const header = this.renderer.selectRootElement("[data-header]");
-  //   const backTopBtn = this.renderer.selectRootElement("[data-back-top-btn]");
-
-  //   if (header && backTopBtn) {
-  //     if (window.scrollY > 100) {
-  //       this.renderer.addClass(header, 'active');
-  //       this.renderer.addClass(backTopBtn, 'active');
-  //     } else {
-  //       this.renderer.removeClass(header, 'active');
-  //       this.renderer.removeClass(backTopBtn, 'active');
-  //     }
-  //   }
-  // }
-  revealElementOnScroll() {
-    const revealElements = document.querySelectorAll<HTMLElement>("[data-reveal]");
-
-    for (let i = 0, len = revealElements.length; i < len; i++) {
-      if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15) {
-        this.renderer.addClass(revealElements[i], 'revealed');
-      } else {
-        this.renderer.removeClass(revealElements[i], 'revealed');
-      }
-    }
-  }
-
-  @HostListener('window:scroll', [])
-  handleScroll() {
-    console.log("Handling scroll");
-    // this.activeElementOnScroll();
-    this.revealElementOnScroll();
-  }
-  addEventOnElements(elements: NodeListOf<Element>, eventType: string, callback: EventListener) {
-    for (let i = 0, len = elements.length; i < len; i++) {
-      elements[i].addEventListener(eventType, callback);
-    }
-  }
-
-
-  setupEventListeners() {
-    // this.addEventOnElements(document.querySelectorAll("[data-nav-toggler]"), "click", this.toggleNav.bind(this));
-    window.addEventListener("scroll", this.handleScroll.bind(this));
-    window.addEventListener("load", this.revealElementOnScroll.bind(this));
-  }
 
   
   
