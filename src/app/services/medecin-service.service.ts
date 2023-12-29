183af9da671cee84a9ae61d0c6e5bb7e52e7b4c1
Naoures <nawreswelhazi@hotmail.com>
2023-12-29 19:54:49 +0100

    Prendre RDV (comboboxs)

---
+++ src/app/services/medecin-service.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Medecin } from 'src/models/medecin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MedecinServiceService {
+
+  public hostMed = 'http://localhost:8080/medecin';
+  public hostRDV= 'http://localhost:8080/rdvs';
+  medecin!: Medecin
+  constructor(private http:HttpClient) { }
+
+  getMedecin(id:number):Observable<Medecin>{
+    return this.http.get<Medecin>(`${this.hostMed + '/get'}/${id}`);
+  }
+
+  loadHeures(id: number, date:string):Observable<string[]>{
+    return this.http.get<string[]>(`${this.hostRDV + '/getRDVMedecinExist'}/${id}/${date}`);
+  }
+}
