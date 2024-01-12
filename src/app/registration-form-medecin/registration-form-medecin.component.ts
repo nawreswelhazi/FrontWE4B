2c242afda482076e55582455d3dca474f087ff62
kamaltechie <ezraidi.kamal.04@gmail.com>
2024-01-12 18:42:59 +0100

    last doctor registration fix

--- src/app/registration-form-medecin/registration-form-medecin.component.ts
+++ src/app/registration-form-medecin/registration-form-medecin.component.ts
@@ -56,8 +56,7 @@ export class RegistrationFormMedecinComponent implements OnInit {
     }
 
     submit() {
-      console.log('Entered codeINE:', this.registrationFormMedecin.value.codeINE);
-      console.log('Valid doctor codes:', this.doctorCodes);
+
         if (this.registrationFormMedecin.value.mdp === this.registrationFormMedecin.value.mdpCheck) {
             const sexe: number = this.registrationFormMedecin.value.sexe === 'homme' ? 0 : 1;
             const enteredCodeINE = this.registrationFormMedecin.value.codeINE;
